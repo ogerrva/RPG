4d0a452d2c74c1fb1ee71838211eb02eebc3bfb7
ogerrva <135985386+ogerrva@users.noreply.github.com>
2026-03-13 17:16:09 -0300

    Refactor service worker for caching assets

--- sw.js
+++ sw.js
@@ -1,24 +1,27 @@
-const CACHE_NAME = 'ear-mage-v1';
-const ASSETS = [
-    './',
-    './index.html',
-    './style.css',
-    './js/data.js',
-    './js/core.js',
-    './js/combat.js',
-    './js/render.js',
-    './js/ui.js',
-    './js/engine.js'
+// sw.js (Service Worker para instalar como App)
+const CACHE_NAME = 'earmage-cache-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './style.css',
+  './js/data.js',
+  './js/core.js',
+  './js/combat.js',
+  './js/render.js',
+  './js/ui.js',
+  './js/engine.js'
 ];
 
-self.addEventListener('install', (e) => {
-    e.waitUntil(
-        caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
-    );
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
 });
 
-self.addEventListener('fetch', (e) => {
-    e.respondWith(
-        caches.match(e.request).then((response) => response || fetch(e.request))
-    );
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
 });

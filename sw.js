f6c632e5558138ae2b8e66fceb7b83e1f88a2a9d
ogerrva <135985386+ogerrva@users.noreply.github.com>
2026-03-25 19:55:45 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,39 +1,8 @@
-const CACHE_NAME = 'twisting-slash-v1';
-const assets = [
-  '/',
-  'index.html',
-  'style.css',
-  'game.js',
-  'https://fonts.googleapis.com/css2?family=Rajdhani:wght@500;600;700&display=swap'
-];
-
-// Instalação: Salva arquivos no cache
-self.addEventListener('install', evt => {
-  evt.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      console.log('Caching assets...');
-      cache.addAll(assets);
-    })
-  );
+self.addEventListener('install', (e) => {
+    console.log('[Service Worker] Instalado');
 });
 
-// Ativação: Limpa caches antigos
-self.addEventListener('activate', evt => {
-  evt.waitUntil(
-    caches.keys().then(keys => {
-      return Promise.all(keys
-        .filter(key => key !== CACHE_NAME)
-        .map(key => caches.delete(key))
-      );
-    })
-  );
-});
-
-// Busca: Serve arquivos do cache se estiver offline
-self.addEventListener('fetch', evt => {
-  evt.respondWith(
-    caches.match(evt.request).then(cacheRes => {
-      return cacheRes || fetch(evt.request);
-    })
-  );
+self.addEventListener('fetch', (e) => {
+    // Apenas passa a requisição adiante (necessário para o PWA ser validado)
+    e.respondWith(fetch(e.request).catch(() => new Response('Offline')));
 });
